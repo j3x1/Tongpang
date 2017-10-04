111731af2ddf24b70b8568ccdca574fda04f81af
Low Jia Jin <pixelrife@hotmail.com>
2017-10-05 01:09:12 +0800

    Added Triggers

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,21 +1,32 @@
-/*create function isAvailable(
-	num_riders int, d_id varchar(64), d_origin varchar(64), d_dest varchar(64), ride_time timestamp
-	)
-returns boolean
-as
-$$
-begin
-    if num_riders <= (select t.space_for from trips t where t.id = d_id and t.origin = d_origin
-    				and t.dest = d_dest and t.ride_time = ride_time and t.status = 'tentative') -
-    				coalesce(sum(select b.num_riders from bids b where b.d_id = d_id and b.d_origin = d_origin
-    				and b.d_dest = d_dest and b.ride_time = ride_time and b.status = 'accepted'), 0) then
-    	return true;
-    else
-    	return false;
-    end if;
-end;
-$$
-language plpgsql;*/
+/*
+create function init_car () returns trigger as $$
+	begin
+	if not(new.company is null and new.model is null and new.status is null) then
+		return null;
+	else
+		new.company = (select u.company from users u where new.id = u.email);
+		new.model = (select u.model from users u where new.id = u.email);
+		new.status = 'tentative';
+		return new;
+	end if;
+	end; $$ language plpgsql;
+
+create trigger init_car before insert on trips
+for each row execute procedure init_car();
+
+create function init_bid() returns trigger as $$
+	begin
+	if new.status is not null then
+		return null;
+	else
+		new.status = 'pending';
+		return new;
+	end if;
+	end; $$ language plpgsql;
+
+create trigger init_bid before insert on bids
+	for each row execute procedure init_bid();
+*/
 
 create table cars(
 	company varchar(64),
@@ -47,8 +58,8 @@ create table trips(
 	dest varchar(64),
 	ride_time timestamp,
 	space_for int not null,
-	company varchar(64) not null,
-	model varchar(64) not null,
+	company varchar(64),
+	model varchar(64),
 	status text check(status in ('tentative','ongoing','completed','cancelled')),
 	primary key(id, origin, dest, ride_time),
 	foreign key(company, model) references cars(company, model)
@@ -64,7 +75,7 @@ create table bids(
 	ride_time timestamp,
 	num_riders int not null,
 	price decimal(10,2) not null,
-	status text check(status in ('accepted','pending','completed','cancelled')),
+	status text check(status in ('accepted','pending','completed','cancelled', 'no show')),
 	foreign key(d_id, d_origin, d_dest, ride_time) 
 	references trips(id, origin, dest, ride_time) on delete cascade on update cascade,
 	primary key(p_id, p_origin, p_dest, d_id, d_origin, d_dest, ride_time),
