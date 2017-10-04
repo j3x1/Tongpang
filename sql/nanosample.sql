edad7c9a81fab94d57589e528b0bca5e7750e1bd
Low Jia Jin <pixelrife@hotmail.com>
2017-10-04 23:56:09 +0800

    Updated Samples and Queries

--- sql/nanosample.sql
+++ sql/nanosample.sql
@@ -12,14 +12,16 @@ insert into languages values ('driver@drive.com', 'English');
 insert into languages values ('driver@drive.com', 'Chinese');
 insert into languages values ('bullet@concorde.com', 'Urdu');
 
+insert into trips values ('driver@drive.com', 'Tanah Merah Station', '30 Havelock Rd', '2015-10-25 08:54:59', 1, 'Nissan', 'Sunny', 'completed');
 insert into trips values ('driver@drive.com', 'Punggol Promenade', 'Sengkang Ave 5', '2015-12-22 18:17:22', 2, 'Nissan', 'Sunny', 'completed');
 insert into trips values ('driver@drive.com', 'Punggol Promenade', 'Sengkang Ave 5', '2016-06-22 19:10:25', 3, 'Nissan', 'Sunny', 'completed');
-insert into trips values ('driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 2, 'Toyota', 'Crown', 'tentative');
+insert into trips values ('driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 3, 'Toyota', 'Crown', 'tentative');
 insert into trips values ('bullet@concorde.com', 'Pasir Ris Park', 'Woodlands St 9', '2017-10-06 12:44:47', 1, 'Ferrari', 'California', 'tentative');
 
+insert into bids values ('nondriver@sit.com', 'Zion Rd', 'River Valley High School', 'driver@drive.com', 'Tanah Merah Station', '30 Havelock Rd', '2015-10-25 08:54:59', 1, 7.20, 'completed');
 insert into bids values ('nondriver@sit.com', 'Blk 122 Punggol West Way', 'Sengkang St 12', 'driver@drive.com', 'Punggol Promenade', 'Sengkang Ave 5', '2015-12-22 18:17:22', 1, 3.00, 'cancelled');
 insert into bids values ('nondriver@sit.com', 'Blk 122 Punggol East Way', 'Sengkang St 12', 'driver@drive.com', 'Punggol Promenade', 'Sengkang Ave 5', '2016-06-22 19:10:25', 1, 3.10, 'completed');
 insert into bids values ('passenger@sit.com', 'Punggol Eastgate', 'Sengkang St 14', 'driver@drive.com', 'Punggol Promenade', 'Sengkang Ave 5', '2016-06-22 19:10:25', 2, 3.20, 'completed');
-insert into bids values ('nondriver@sit.com', 'Changi Village Hawker Centre', 'Sengkang East Drive', 'driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 1, 5.50, 'pending');
-insert into bids values ('passenger@sit.com', 'Changi Drive 12', 'Sengkang Mall', 'driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 2, 7.50, 'pending');
+insert into bids values ('nondriver@sit.com', 'Changi Village Hawker Centre', 'Sengkang East Drive', 'driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 2, 5.50, 'pending');
+insert into bids values ('passenger@sit.com', 'Changi Drive 12', 'Sengkang Mall', 'driver@drive.com', 'Changi Airport T1', 'Sengkang Ave 5', '2017-10-06 11:05:13', 1, 7.50, 'accepted');
 insert into bids values ('nondriver@sit.com', 'Pasir Ris MRT', 'Woodlands Bus Depot', 'bullet@concorde.com', 'Pasir Ris Park', 'Woodlands St 9', '2017-10-06 12:44:47', 1, 15.50, 'pending');
--- sql/queries.sql
+++ sql/queries.sql
@@ -5,16 +5,24 @@ select language from languages where email = MY_EMAIL;
 select t.origin, t.dest, t.ride_time, coalesce(sum(b.price), 0.00) earned from trips t left join bids b on
 	t.id = b.d_id and t.origin = b.d_origin and t.dest = b.d_dest and t.ride_time = b.ride_time and b.status = 'completed'
 where t.id = MY_EMAIL and t.status = 'completed'
-group by t.id, t.origin, t.dest, t.ride_time;
+group by t.id, t.origin, t.dest, t.ride_time
+order by t.ride_time desc;
 
 /* All trips taken */
-select p_origin, p_dest, ride_time, price from bids where r_id = MY_EMAIL and status = 'completed';
+select p_origin, p_dest, ride_time, price from bids where r_id = MY_EMAIL and status = 'completed' order by ride_time desc;
 
-/* All relevant trips that can be bid for (ride has yet to start, not driven by oneself, and still has space left*/
+/* All relevant trips that can be bid for (ride has yet to start, not driven by oneself, still has space left, and starting/ending in same place)*/
 select t.id, t.origin, t.dest, t.ride_time from trips t 
 where t.status = 'tentative' and t.id != MY_ID and 
-	coalesce(sum(select b.num_riders from bids b where b.d_id = d_id and b.d_origin = t.d_origin and b.d_dest = t.d_dest
-	and b.ride_time = t.ride_time and b.status = 'accepted'), 0) + NUM_RIDERS <= t.space_for
+	(select coalesce(sum(riders_so_far), 0) from 
+    	(select b.num_riders riders_so_far from bids b where b.d_id = t.id and b.d_origin = t.origin 
+    	and b.d_dest = t.dest and b.ride_time = t.ride_time and b.status = 'accepted') co_riders) + NUM_MY_RIDERS <= t.space_for
 	and (t.origin like '%MY_ORIGIN%' or t.dest like '%MY_DEST%');
 
-/* All active bids for a ride */
+/* All pending bids for a tentative/ongoing trip */
+select b.p_id, b.p_origin, b.p_dest, b.ride_time, b.price from bids b
+where b.d_id = MY_ID and b.d_origin = MY_ORIGIN and b.d_dest = MY_DEST and b.ride_time = MY_RIDE_TIME and b.status = 'pending';
+
+/* All accepted bids for a tentative/ongoing trip */
+select b.p_id, b.p_origin, b.p_dest, b.ride_time, b.price from bids b
+where b.d_id = MY_ID and b.d_origin = MY_ORIGIN and b.d_dest = MY_DEST and b.ride_time = MY_RIDE_TIME and b.status = 'accepted';
