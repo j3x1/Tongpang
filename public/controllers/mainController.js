67186e7ebc80b97f0f2a42f69cba29ca8770dc5c
CJX3711 <chaijiaxun@gmail.com>
2017-10-05 03:03:08 +0800

    Working but erroroeoauoanous

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -18,16 +18,19 @@ app.config(function($routeProvider) {
 app.controller('LoginController', ['$scope', '$http', function($scope, $http) {
   $scope.title = "Tongpang";
   $scope.welcome = "Welcome destination tongpang, where you can tongpang stuff.";
-  
-  $scope.click = function() {
-    console.log("Hello");
-    
+  $scope.loginEmail = "";
+  $scope.login = function(redirURL) {
     $http({
       method: 'GET',
-      url: '/dbTest'
+      url: '/login/' + $scope.loginEmail
     }).then(function successCallback(response) {
-      console.log(response.data);
-      $scope.title = "Doneee";
+      console.log(response.data.email);
+      if ( response.data.email === $scope.loginEmail ) {
+        window.localStorage.setItem("user", $scope.loginEmail);
+        window.location.href = '/#!/' + redirURL;
+      } else {
+        $scope.errorMessage = "Invalid email";
+      }
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
@@ -38,37 +41,57 @@ app.controller('LoginController', ['$scope', '$http', function($scope, $http) {
 
 app.controller('DriverController', ['$scope', '$http', function($scope, $http) {
   $scope.driverName = "Driver";
-  
+  $scope.email = window.localStorage.getItem("user");
+
+  if ( !$scope.email ) {
+    window.location.href = '/#!/';
+  }
   $scope.resetForm = function() {
-    $scope.addForm.name = "";
     $scope.addForm.spaces = 0;
+    $scope.addForm.origin = "";
     $scope.addForm.destination = "";
+    $scope.addForm.time = "2017-10-06 11:05:13";
   }
   $scope.showAddForm = false;
   
   $scope.addForm = {}
   $scope.resetForm();
   
-  $scope.tempID = 3;
-  $scope.carList = [
-    {
-      "id": 1,
-      "name": "Red Honda",
-      "spaces": 2,
-      "destination": "Bedok"
-    },
-    {
-      "id": 2,
-      "name": "Blue Toyota",
-      "spaces": 1,
-      "destination": "Serangoon"
-    }
-  ]
+  $scope.rideHistory = [];
+
+  
+  $scope.logout = function() {
+    localStorage.clear();
+    window.location.href = '/#!/';  
+  }
+  
+  $scope.getRides = function() {
+    $http({
+      method: 'GET',
+      url: '/user/' + $scope.email + '/drivehistory'
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      $scope.rideHistory = response.data;
+    }, function errorCallback(response) {
+    });
+  }
+
+  $scope.getRides();
+  
+  $scope.addDisabled = function() {
+    return false;
+  }
+
   
   $scope.addTrip = function() {
-      var newCar = JSON.parse(JSON.stringify($scope.addForm));
-      newCar.id = $scope.tempID++;
-      $scope.carList.push(newCar);
+      $http({
+        method: 'POST',
+        url: '/user/' + $scope.email + '/drive',
+        data: JSON.parse(JSON.stringify($scope.addForm))
+      }).then(function successCallback(response) {
+        console.log(response.data);
+      }, function errorCallback(response) {
+      });
       $scope.resetForm();
   }
   
